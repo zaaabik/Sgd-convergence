2b1b4110f0edc7def0c916d977d8622e44e8825a
zabik <zaaabik@gmail.com>
2020-04-01 23:08:04 +0300

    function to generating data and weights

--- helper.py
+++ helper.py
@@ -1,14 +1,13 @@
-import numpy as np
+import torch
+from torch import nn
 
-in_dim = 100
-tr_size = 1e5
-batch_size = 200
-step = 1e-3
 
-W = np.random.normal(1,1,(in_dim,in_dim))
-W = 5*W/np.linalg.norm(W)
-np.linalg.norm(W)
+def init_weights(model, size=100):
+    weight = torch.FloatTensor(size, size).normal_()
+    weight = 5 * weight / torch.norm(weight)
+    model.linear.weight = nn.Parameter(weight)
+    return model
 
-x = np.random.normal(1,1,in_dim)
-x = x/np.linalg.norm(x)
-np.linalg.norm(x)
+
+def create_input(dim, size=100):
+    return torch.FloatTensor(dim, size).normal_()
