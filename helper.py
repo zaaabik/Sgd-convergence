9934291137b49c821a3cea7094e7726e89344716
kechua <kechua98@gmail.com>
2020-04-01 22:33:33 +0300

    helper -> .py

---
+++ helper.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+in_dim = 100
+tr_size = 1e5
+batch_size = 200
+step = 1e-3
+
+W = np.random.normal(1,1,(in_dim,in_dim))
+W = 5*W/np.linalg.norm(W)
+np.linalg.norm(W)
+
+x = np.random.normal(1,1,in_dim)
+x = x/np.linalg.norm(x)
+np.linalg.norm(x)
